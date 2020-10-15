98ddb2a4e85365e4deb780b625ee5fedb1f7df44
n0vaes <alanovaes@live.com>
2020-10-14 21:50:14 -0300

    Ajustes backend

--- src/models/Image.ts
+++ src/models/Image.ts
@@ -1,5 +1,4 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
-import orphanage from './Orphanage';
 import Orphanage from './Orphanage';
 
 @Entity('images')
